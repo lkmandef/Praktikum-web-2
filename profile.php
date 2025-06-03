71bb6da2b31eaf7c16b002368c3509a346b6159f
lkmandef <lukmanoman17@gmail.com>
2025-06-03 09:18:54 +0800

    Initial commit

---
+++ profile.php
@@ -0,0 +1,52 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/dedy.jpg" alt="Dedy Rosyadi" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2310010398</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Lukmanul Hakim</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Berangas Timur, 21 Agustus 2004</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">085604537002</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">lukmanoman17@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/mdedyrosyadi" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/facebook.png" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/lkman_u" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/instagram.png" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/dedyrosyadi" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/twitter.png" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/dedyrosyadi" target="_blank">
+                                <img src="img/medsos/linkedin.png" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
